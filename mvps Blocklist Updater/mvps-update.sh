7ae5353662cfcf74ba185b83eaa52d73ca6f46b2
genghizkhan91 <savya.jha91@gmail.com>
2014-08-19 19:42:33 +0530

    Added comments and a single item to whitelist

--- mvps Blocklist Updater/mvps-update.sh
+++ mvps Blocklist Updater/mvps-update.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
 
+# Downloads the blovklists
 /usr/bin/wget -q -O "/tmp/blocklist-tmp.txt.1" "http://hosts-file.net/.%5Cad_servers.txt" &> /dev/null
 /usr/bin/wget -q -O "/tmp/blocklist-tmp.txt.2" "http://adaway.org/hosts.txt" &> /dev/null
 /usr/bin/wget -q -O "/tmp/blocklist-tmp.txt.3" "http://winhelp2002.mvps.org/hosts.txt" &> /dev/null
+
+# Concatenates the different lists
 cat "/tmp/blocklist-tmp.txt.1" "/tmp/blocklist-tmp.txt.2" "/tmp/blocklist-tmp.txt.3" | sed -e "s/127.0.0.1/0.0.0.0/g" -e 's/#.*//g' -e '/^#/d' | sort | uniq > "/tmp/blocklist-tmp.txt"
+
+# Add your own whitelisting rules here using sed
+sed -i '/use.typekit.net/d' "/tmp/blocklist-tmp.txt"
+
+# Copy the tmpfile to /etc
 if [ -e "/tmp/blocklist-tmp.txt" ]; then
     mv "/etc/hosts.block" "/etc/hosts-block.bak"
     mv "/tmp/blocklist-tmp.txt" "/etc/hosts.block"
